bb5479f1b61f6b0f50eb85ae90d0b3b9ea5ea058
Júlio Olmo <souzaolmo@gmail.com>
2023-05-22 18:58:10 -0300

    Up

--- aula30.py
+++ aula30.py
@@ -1,3 +1,10 @@
+velocidade = 61
+local_carro = 100
+
+RADAR_1 = 60  # velocidade máxima do radar 1
+LOCAL_1 = 100  # local onde o radar 1 está
+RADAR_RANGE = 1  # A distância onde o radar pega
+
 vel_carro_pass_radar_1 = velocidade > RADAR_1
 carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and \
     local_carro <= (LOCAL_1 + RADAR_RANGE)
