e3f23986bc01f48c0dc0e6d82541778188634cfc
Júlio Olmo <souzaolmo@gmail.com>
2023-05-29 10:51:53 -0300

    exercicios resolvidos

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,23 +1,12 @@
-entrada = input('digite um numero: ') 
-if entrada.isdigit():
-    entrada_int= int(entrada)
-    par_impar = entrada_int %2 == 0
-    par_impar_texto = 'ímpar'
+nome = input('digite seu primeiro nome: ')
+tamanho_nome = len(nome)
 
-    if par_impar:
-        par_impar_texto = 'par'
-
-        print(f'O número{entrada_int} é {par_impar_texto}')
+if tamanho_nome > 1:
+    if tamanho_nome <=4:
+        print('Seu nome é curto')
+    elif tamanho_nome >=5 and tamanho_nome <=6:
+        print('seu nome é normal')
+    else:
+        print('seu nome é muito grande')
 else:
-    print('você não digitou um numero inteiro')  
-try:
-    entrada_int = float(entrada)
-    par_impar = entrada_int %2 == 0
-    par_impar_texto = 'ímpar'
-
-    if par_impar:
-        par_impar_texto = 'par'
-        
-         print(f'O número {entrada_int} é {par_impar_texto}')
-except:
-    print('Você não digitou um número inteiro')
+    print('não conheço esse nome')
