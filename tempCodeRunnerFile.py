ecdac48501fda1e7917137983be07813fa3a5fff
Júlio Olmo <souzaolmo@gmail.com>
2023-05-31 15:02:29 -0300

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,7 @@
-nome = input('digite seu primeiro nome: ')
-tamanho_nome = len(nome)
+contador = 0
 
-if tamanho_nome > 1:
-    if tamanho_nome <=4:
-        print('Seu nome é curto')
-    elif tamanho_nome >=5 and tamanho_nome <=6:
-        print('seu nome é normal')
-    else:
-        print('seu nome é muito grande')
-else:
-    print('não conheço esse nome')
+while contador <= 9:
+    contador = contador + 2
+    print(contador)
+
+print('Acabou')
