cb00a1a102a6fb7bb1593f79cd30eda2bbf68dda
Júlio Olmo <souzaolmo@gmail.com>
2023-06-19 08:31:17 -0300

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,34 +1,11 @@
-while True:
-    numero_1 = input('Digite um número: ')
-    numero_2 = input('Digite outro número: ')
-    operador = input('Digite o operador (+-/*): ')
+frase = 'aaaooo'
 
-    numeros_validos = None
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
 
-    try:
-        num_1_float = float(numero_1)
-        num_2_float = float(numero_2)
-        numeros_validos = True
-    except:
-        numeros_validos = None
+while i < len(frase):
+    letra_atual = frase[i]
 
-    if numeros_validos is None:
-        print('Um ou ambos os números digitados são inválidos.')
-        continue
-
-    operadores_permitidos = '+-/*'
-
-    if operador not in operadores_permitidos:
-        print('Operador inválido.')
-        continue
-
-    if len(operador) > 1:
-        print('Digite apenas um operador.')
-        continue
-
-    ###
-
-    sair = input('Quer sair? [s]im: ').lower().startswith('s')
-
-    if sair is True:
-        break
+    if letra_atual == ' ':
+        i += 1
