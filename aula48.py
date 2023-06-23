fdfaaf73422b3a9c7cebdd9d437fb8689f47f8b8
Júlio Olmo <souzaolmo@gmail.com>
2023-06-23 14:33:03 -0300

    Lista em Python

--- aula48.py
+++ aula48.py
@@ -17,3 +17,26 @@ lista = [123, True, 'Luiz Otávio',  1.2, []]
 lista[-3] = 'Maria'
 print(lista)
 print(lista[2], type(lista[2]))
+
+
+'''Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3   4   5
+lista = [10, 20, 30, 40]
+# lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+lista.append(50)
+lista.pop()
+lista.append(60)
+lista.append(70)
+ultimo_valor = lista.pop(3)
+print(lista, 'Removido,', ultimo_valor)
