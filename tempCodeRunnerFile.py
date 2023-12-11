63d0d97b3ba6731fa6f66f32de309051db9f630b
Júlio Olmo <souzaolmo@gmail.com>
2023-12-10 22:23:36 -0300

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,23 @@
-Exercício
-Exiba os índices da lista
-0 Maria
-1 Helena
-2 Luiz
-"""
-lista = ['Maria', 'Helena', 'Luiz']
-lista.append('João')
+entrada = input('digite um numero: ') 
+if entrada.isdigit():
+    entrada_int= int(entrada)
+    par_impar = entrada_int %2 == 0
+    par_impar_texto = 'ímpar'
 
+    if par_impar:
+        par_impar_texto = 'par'
 
-indices = range(len(lista))
+        print(f'O número{entrada_int} é {par_impar_texto}')
+else:
+    print('você não digitou um numero inteiro')  
+try:
+    entrada_int = float(entrada)
+    par_impar = entrada_int % 2 == 0
+    par_impar_texto = 'ímpar'
 
-for indice in indices:
-    print(indice, lista[indice], type(lista[indice]))
+    if par_impar:
+        par_impar_texto = 'par'
+
+         print(f'O número {entrada_int} é {par_impar_texto}')
+except:
+    print('Você não digitou um número inteiro')
